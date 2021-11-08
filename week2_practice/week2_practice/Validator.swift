34738c9d46e14d906c0f2e606ae8d08ab1e4ecef
bumgeunsong <isp1195@gmaill.com>
2021-11-08 17:49:58 +0900

    Add solution #4 socialNumberValidator on week2 Monday

--- week2_practice/week2_practice/Validator.swift
+++ week2_practice/week2_practice/Validator.swift
@@ -16,7 +16,7 @@ struct Validator {
         if id.count < 5 || id.count > 24 {
             return false
         }
-
+        
         // 영문 대소문자, 숫자, -만 포함 가능하고 그 외에는 실패
         for char in id {
             if !char.isLetter && !char.isNumber && !(char.asciiValue! == 45) {
@@ -29,11 +29,11 @@ struct Validator {
         // 숫자가 연번으로 3개나오면 실패 123, 234
         
         // String의 한 글자씩 loop를 돌리면서, 해당 string의 앞글자 (현재 index - 1)과 다음 글자 (현재 index + 1) 에 접근할 수 있는 방법이 뭐가 있을까?
-//        for (index, char) in id.enumerated() {
-//            print(id[index + 1])
-//        }
-//
-//
+        //        for (index, char) in id.enumerated() {
+        //            print(id[index + 1])
+        //        }
+        //
+        //
         
         
         var idArray: [Int] = []
@@ -60,7 +60,7 @@ struct Validator {
     
     func passwordValidator(password: String) -> Int {
         var level = 3
-
+        
         if password.count < 8 {
             if password.range(of: "^[0-9]*$", options: .regularExpression) != nil {
                 level = 1
@@ -77,6 +77,31 @@ struct Validator {
             }
         }
         return level
+    }
+    
+    func socialNumberValidator(socialNumber: String) -> Bool {
+        if socialNumber.range(of: "^[0-9-]*$", options: .regularExpression) != nil {
+            let nums = socialNumber.filter { char in
+                return char.isNumber
+            }
+            
+            var index = 0
+            let multiplier = [2,3,4,5,6,7,8,9,2,3,4,5]
+            var result = nums.dropLast().reduce(0) { acc, cur in
+                let sum = acc + (cur.wholeNumberValue! * multiplier[index])
+                index += 1
+                return sum
+            }
+            
+            result = (11 - (result % 11)) % 10
+            
+            if result == socialNumber.last?.wholeNumberValue {
+                return true
+            }
+        } else {
+            print("Not a valid format!")
+        }
+        return false
     }
     
 }
