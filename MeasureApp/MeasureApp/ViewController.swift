339c54f7898c1aab2141532c26f30f46be223f21
bumgeunsong <isp1195@gmaill.com>
2021-11-09 17:32:52 +0900

    Create converter

--- MeasureApp/MeasureApp/ViewController.swift
+++ MeasureApp/MeasureApp/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var valueTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
-
-
+    
+    @IBAction func convertPressed(_ sender: UIButton) {
+        if let valueString = valueTextField.text {
+            if let value = Double(valueString) {
+                let result = Measurement(value: value, unit: UnitLength.inches)
+                    .converted(to: UnitLength.meters)
+                    .formatted()
+                resultLabel.text = result
+            }
+        }
+    }
 }
 
