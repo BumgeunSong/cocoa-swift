e2a5926fe48991801f4bff570def780121919782
bumgeunsong <isp1195@gmaill.com>
2021-11-10 15:01:42 +0900

    Create struct for problem 1

---
+++ week2_practice/week2_practice/DepositManager.swift
@@ -0,0 +1,22 @@
+//
+//  DepositManager.swift
+//  week2_practice
+//
+//  Created by Bumgeun Song on 2021/11/10.
+//
+
+import Foundation
+
+struct DepositManager {
+    func getInterestRate(byDay: Int) -> Double {
+        
+    }
+    
+    func calculateAmount(dat: Int, amount: Int) -> Double {
+        
+    }
+    
+    func testInterest(unitDay: Int) -> Double {
+        
+    }
+}
