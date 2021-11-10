0bf8ea831c2518fc72605c9b8c2244fcfe6a2a7e
bumgeunsong <isp1195@gmaill.com>
2021-11-10 15:08:57 +0900

    Add Solution for problem 1 getInterestRate()

--- week2_practice/week2_practice/DepositManager.swift
+++ week2_practice/week2_practice/DepositManager.swift
@@ -9,14 +9,25 @@ import Foundation
 
 struct DepositManager {
     func getInterestRate(byDay: Int) -> Double {
-        
+        switch byDay {
+        case 1...90:
+            return Double(0.005)
+        case 91...180:
+            return Double(0.01)
+        case 181..<365:
+            return Double(0.02)
+        case 365...:
+            return Double(0.056)
+        default:
+            return Double(0)
+        }
     }
     
     func calculateAmount(dat: Int, amount: Int) -> Double {
-        
+        return 0.0
     }
     
     func testInterest(unitDay: Int) -> Double {
-        
+        return 0.0
     }
 }
--- week2_practice/week2_practice/main.swift
+++ week2_practice/week2_practice/main.swift
@@ -9,11 +9,21 @@ import Foundation
 
 var myValidator = Validator()
 
-//MARK: - test case: IDValidator
+//MARK: - test case: IDValidator()
 
-print(myValidator.IDValidator(id: "helo23yo45"), "should be true")
-print(myValidator.IDValidator(id: "helo23yo456"), "should be false")
-print(myValidator.IDValidator(id: "isp555-23"), "should be false")
-print(myValidator.IDValidator(id: "isp5-5523"), "should be true")
+//print(myValidator.IDValidator(id: "helo23yo45"), "should be true")
+//print(myValidator.IDValidator(id: "helo23yo456"), "should be false")
+//print(myValidator.IDValidator(id: "isp555-23"), "should be false")
+//print(myValidator.IDValidator(id: "isp5-5523"), "should be true")
+
+//MARK: - test case: getInterestRate()
+
+var myDepositeManager = DepositManager()
+print(myDepositeManager.getInterestRate(byDay: 0))
+print(myDepositeManager.getInterestRate(byDay: 90))
+print(myDepositeManager.getInterestRate(byDay: 91))
+print(myDepositeManager.getInterestRate(byDay: 181))
+print(myDepositeManager.getInterestRate(byDay: 365))
+print(myDepositeManager.getInterestRate(byDay: -365))
 
 
