6947ead8cd44af0a889fba93f82eb6a29674a951
BumgeunSong <isp1195@gmail.com>
2021-11-15 14:31:00 +0900

    Create solution for problem#2: Binary

---
+++ week3_practice/week3_practice/Binary.swift
@@ -0,0 +1,52 @@
+//
+//  Binary.swift
+//  week3_practice
+//
+//  Created by Bumgeun Song on 2021/11/15.
+//
+
+import Foundation
+
+class Binary {
+    private var values: Array<String>
+    
+    init(with total: Int) {
+        // total 값과 같은 비트 길이를 가지는 2진수 숫자를 생성해서 내부 private values 배열에 초기화한다.
+        var result: [String] = []
+        func recursion(input: String) {
+            if input.count >= 5 {
+                result.append(input)
+                return
+            }
+            recursion(input: "\(input)0")
+            recursion(input: "\(input)1")
+            return
+        }
+        recursion(input: "")
+        values = result
+    }
+    
+    var count: Int {
+        return values.count
+    }
+    
+    func find(by bitcount: Int) -> Array<String> {
+        // 초기화한 values 배열 값 중에서 1인 비트 개수가 bitcount개 있는 값만 배열에 문자열로 담아서 return 한다.
+        return values.filter { binary in
+            let itemCount = binary.reduce(0) { partialResult, bit in
+                return bit == Character("1") ? partialResult + 1 : partialResult
+            }
+            return itemCount == bitcount
+        }
+        
+    }
+}
+
+extension Binary: CustomStringConvertible {
+    var description: String {
+        let string = values.reduce("") { partialResult, value in
+            return "\(partialResult)'\(value)',"
+        }
+        return "[\(string)]"
+    }
+}
