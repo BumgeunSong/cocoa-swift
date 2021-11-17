b4f83bebe222d3bc08d69017d886e5b3e84ac92f
BumgeunSong <isp1195@gmail.com>
2021-11-17 17:35:41 +0900

    Add CalculatorLogic

---
+++ CalculatorApp/CalculatorApp/CalculatorLogic.swift
@@ -0,0 +1,27 @@
+//
+//  CalculatorLogic.swift
+//  CalculatorApp
+//
+//  Created by Bumgeun Song on 2021/11/17.
+//
+
+import Foundation
+
+struct CalculatorLogic {
+    
+    func calculate(_ n1: Double, _ n2: Double, op: String) -> Double? {
+        switch op {
+        case "+":
+            return n1 + n2
+        case "−":
+            return n1 - n2
+        case "×":
+            return n1 * n2
+        case "÷":
+            return n1 / n2
+        default:
+            return nil
+        }
+        
+    }
+}
--- CalculatorApp/CalculatorApp/ViewController.swift
+++ CalculatorApp/CalculatorApp/ViewController.swift
@@ -8,17 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var displayLabel: UILabel!
+
+    
+    @IBOutlet var stacks: [UIButton]!
+    @IBOutlet var opStacks: [UIButton]!
+    
+    var stackPointer = 0
+    var calculatorLogic = CalculatorLogic()
+    
+    var previousOperator: String?
+    var previousNumber: Double?
+    
     var isTypingNumber: Bool = false
+    var currentNumber: Double {
+        guard let number = Double(displayLabel.text!) else {
+            fatalError("Cannot convert Text to Number.")
+        }
+        return number
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
     @IBAction func numPressed(_ sender: UIButton) {
-        guard let input = sender.titleLabel?.text else {
+        guard let numInput = sender.titleLabel?.text else {
             print("Invalid Input")
             return
         }
@@ -28,22 +46,64 @@ class ViewController: UIViewController {
             return
         }
         
-        if input == "." && displayLabel.text!.contains(".") {
+        if numInput == "." && displayLabel.text!.contains(".") {
             print("Cannot use Decimal point when number is not Integer")
             return
         }
         
+        if stackPointer >= 5 {
+            print("Stack is Full")
+            return
+        }
+        
         if isTypingNumber {
-            displayLabel.text! += input
+            displayLabel.text! += numInput
         } else {
-            displayLabel.text = input
+            displayLabel.text = numInput
             isTypingNumber = true
         }
         
     }
     
     @IBAction func operatorPressed(_ sender: UIButton) {
+        guard let opInput = sender.titleLabel?.text else {
+            print("Invalid Input")
+            return
+        }
+        
+        if previousOperator == nil {
+            if opInput == "=" {
+                print("Cannot use = operator without other operator")
+                return
+            }
+            previousNumber = currentNumber
+            previousOperator = opInput
+            updateStacks(isDone: false)
+            
+        } else {
+            guard let prev = previousNumber, let prevOp = previousOperator else {
+                fatalError("No previsous number & operator")
+            }
+            
+            guard let results = calculatorLogic.calculate(prev, currentNumber, op: prevOp) else {
+                fatalError("Cannot caculate with \(prevOp)")
+            }
+            
+            previousNumber = results
+            previousOperator = opInput
+            updateStacks(isDone: false)
+            
+            if opInput == "=" {
+                updateStacks(isDone: true)
+                previousOperator = nil
+                stackPointer += 1
+            }
+        }
+
+        displayLabel.text = "0"
+        isTypingNumber = false
     }
+
     
     // MARK: - Handling Gesture
     
@@ -55,6 +115,56 @@ class ViewController: UIViewController {
         if motion == .motionShake {
             displayLabel.text = "0"
             isTypingNumber = false
+            stackPointer = 0
+            resetStacks()
+        }
+    }
+    
+    func updateStacks(isDone: Bool) {
+        if isDone {
+            for stack in stacks {
+                stack.isEnabled = false
+            }
+            
+            for opStack in opStacks {
+                opStack.isEnabled = false
+                if opStack.tag == stackPointer {
+                    opStack.setTitle("", for: .normal)
+                }
+            }
+        } else {
+            for stack in stacks {
+                if stack.tag == stackPointer {
+                    stack.setTitle(String(previousNumber!), for: .normal)
+                    stack.isEnabled = true
+                } else {
+                    stack.isEnabled = false
+                }
+                
+            }
+            
+            for opStack in opStacks {
+                if opStack.tag == stackPointer {
+                    opStack.setTitle(previousOperator, for: .normal)
+                    opStack.isEnabled = true
+                } else {
+                    opStack.isEnabled = false
+                }
+            }
+        }
+        
+    }
+    
+    func resetStacks() {
+        for n in 0..<stacks.count {
+            stacks[n].isEnabled = false
+            stacks[n].setTitle("Stack\(n+1)", for: .normal)
+            stacks[n].titleLabel?.font = UIFont.systemFont(ofSize: 30)
+        }
+        
+        for opStack in opStacks {
+            opStack.isEnabled = false
+            opStack.setTitle("", for: .normal)
         }
     }
     
